4798b9af4f3b782292164fc3a27fa8162d4f62dc
Diego Barros <diego.barros.flf@gmail.com>
2020-04-13 21:08:39 -0300

    Crio menu de opções da agenda.

--- App.java
+++ App.java
@@ -1,14 +1,44 @@
+import java.util.Scanner;
+
 public class App{
 	public static void main(String[] args) {
 		
 		Agenda agenda = new Agenda();
+		Scanner teclado = new Scanner(System.in);
+		int menu = 0;
+		boolean loop = true;
 
-		System.out.println("Agenda 2020\n");
-
-		// for(Contato c : agenda.getContatos()){
-		// 	c.mostrarContato();
-		// }
 		
+
+		while(loop){
+			System.out.println("Agenda 2020\n");
+
+			System.out.println("(1) Pesquisar");
+			System.out.println("(2) Adicionar");
+			System.out.println("(3) Deletar");
+			System.out.println("(4) Editar");
+			System.out.println("(5) Listar");
+			System.out.println("(6) Sair\n");
+
+			System.out.println("Escolha uma opção do menu.");
+
+			menu = teclado.nextInt();
+
+			//esvaziar o buffer do teclado
+			teclado.nextLine();
+
+
+			switch(menu){
+
+				case 6:
+					System.out.println("Saindo...");
+					teclado.close();
+					loop = false;
+			}
+
+
+		}
+
 
 
 
